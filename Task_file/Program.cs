9b491428245ae5cd885d26dd581d2e8bf0c99f9e
Radmir Shagiev <veselday@gmail.com>
2023-06-25 08:54:05 +0500

    creating first option of Task_file

--- Task_file/Program.cs
+++ Task_file/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿/*Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых либо меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма.
+При решение не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*/
+
+Console.Write("Please write the array size ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] firstArray = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Please write {i+1} element: ");
+    string count = Convert.ToString(Console.ReadLine());
+    firstArray[i] = count;
+}
+
+string[] finalArray = new string[size];
+int lenArray = 3;
+int posArray = 0;
+
+for (int j = 0; j < size; j++)
+{
+    if (firstArray[j].Length <= lenArray)
+    {
+        finalArray[posArray] = firstArray[j];
+        posArray++;
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+PrintArray(finalArray);
